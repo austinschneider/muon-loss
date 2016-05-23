104be28ab090100e90c6ac763ad502e260c97b1f
Austin Schneider <hogenshpogen@gmail.com>
2016-05-23 17:09:22 -0500

    Added weight information.

--- mc-proc/plot_pkl.py
+++ mc-proc/plot_pkl.py
@@ -3,32 +3,36 @@ import matplotlib.pyplot as plt
 
 import pickle
 import glob
-import argparse  
+import argparse
+import itertools
 
 parser = argparse.ArgumentParser(description='Proccess filenames')
 
 parser.add_argument('indirs', metavar='indirs',  nargs='+')
-#parser.add_argument('-g', '--geo', default='')
-#parser.add_argument('-o', '--outfile', default='')
 
 args = parser.parse_args()
 indirs = args.indirs
 
 infiles = [file  for indir in indirs for file in glob.glob('%s*.pkl' % indir)]
+infiles.sort()
 
 fractional_losses = []
+weights = []
 
 for infile in infiles:
     inpickle = open(infile, 'rb')
     fractional_losses += pickle.load(inpickle)
+    weights += pickle.load(inpickle)
     inpickle.close()
 
 max_distance = 600.
 
-bin_it = np.linspace(0, max_distance, 31)
-#hists = [np.array([sum([loss for loss,dist in itertools.izip(frac_loss[0], frac_loss[1]) if dist >= bin and dist < bin+20]) for bin in bin_it]) for frac_loss in fractional_losses]
-hists = [np.array([sum([loss for loss,dist in frac_loss if dist >= bin and dist < bin+20]) for bin in bin_it]) for frac_loss in fractional_losses]
-single_hist = sum(hists) / len(hists)
+bin_size = 60
+
+bin_it = np.linspace(0, max_distance, max_distance / bin_size + 1)
+hists = [np.array([sum([loss for loss,dist in frac_loss if dist >= bin and dist < bin+bin_size]) for bin in bin_it]) for frac_loss in fractional_losses]
+weighted_hists = [hist*weight for hist,weight in itertools.izip(hists, weights)]
+single_hist = sum(weighted_hists) / sum(weights)
 plt.hist(bin_it, bins=bin_it, weights=single_hist, color='b', histtype='step')
 plt.title('Fractional muon energy loss along track')
 plt.xlabel('Track distance (m)')
