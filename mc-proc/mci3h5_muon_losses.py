bbbcb447911adbe7e6acfe2a5ee4f105374aa44e
Austin Schneider <hogenshpogen@gmail.com>
2016-05-25 16:31:38 -0500

    Changed to use track information and store in multiple objects.

--- mc-proc/mci3h5_muon_losses.py
+++ mc-proc/mci3h5_muon_losses.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import pdb
+
 import sys
 import itertools
 import glob
@@ -9,6 +11,7 @@ import ntpath
 from icecube import icetray, dataio
 from icecube import dataclasses
 from icecube import tableio, hdfwriter
+from icecube import simclasses
 
 from I3Tray import *
 
@@ -22,6 +25,7 @@ tray = I3Tray()
 
 import argparse
 
+# Initialize arg parser
 parser = argparse.ArgumentParser(description='Proccess filenames')
 
 parser.add_argument('indirs', metavar='indirs',  nargs='+')
@@ -35,6 +39,10 @@ geo = args.geo
 outdir = args.outdir
 bundle_size = args.bundlesize
 
+def unique_list(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 # Get files aleady processed by looking at the output directory
 # Output files have same similar filename structure to input files
@@ -44,8 +52,8 @@ def get_processed_files(dir):
     files = glob.glob('%s*.h5' % dir)
     for f in files:
         file_source = ntpath.splitext(ntpath.basename(f))[0] + '.i3.bz2'
-        if('-' in file_source.split('.')[3]):
-            source_nums = (lambda x: range(int(x[0]), int(x[1])+1))(file_source.split('.')[3].split('-')) #Filename has number after 3rd period
+        if('-' in file_source.split('.')[3]): # If there is a number range
+            source_nums = (lambda x: range(int(x[0]), int(x[1])+1))(file_source.split('.')[3].split('-')) # Filename has number after 3rd period
             for n in source_nums:
                 source_split = file_source.split('.')
                 source_split[3] = '%06.0f' % n #Filename has number after third period
@@ -56,6 +64,20 @@ def get_processed_files(dir):
                                                                                
     return processed_files
 
+# Get input files that have no already been processed
+def get_infiles(indirs, outdir):
+    processed_files = get_processed_files(outdir)
+    infiles = [file for indir in indirs for file in glob.glob('%s*.i3.bz2' % indir) if ntpath.basename(file) not in processed_files]
+    infiles.sort()
+    return infiles
+    
+
+# Define particle sets
+p = dataclasses.I3Particle()
+loss_set = set([p.PairProd, p.DeltaE, p.Brems, p.NuclInt])
+nu_set = set([p.Nu, p.NuE, p.NuEBar, p.NuMu, p.NuMuBar, p.NuTau, p.NuTauBar])
+mu_set = set([p.MuPlus, p.MuMinus])
+
 # Store 3 kinds of particles
 #   Primary neutrino
 #   Highest energy muon daughter
@@ -63,75 +85,77 @@ def get_processed_files(dir):
 class MyModule(icetray.I3ConditionalModule):
     def __init__(self, context):
         super(MyModule, self).__init__(context)
-    def get_energy(self, particle):
-        return particle.energy
+    def get_energy(self, track):
+        return track.particle.energy
     def Configure(self):
-        p = dataclasses.I3Particle()
-        self.loss_set = set([p.PairProd, p.DeltaE, p.Brems, p.NuclInt])
-        self.nu_set = set([p.Nu, p.NuE, p.NuEBar, p.NuMu, p.NuMuBar, p.NuTau, p.NuTauBar])
-    def DAQ(self, frame):
+        pass
+    def Physics(self, frame):
         Tree = frame['I3MCTree']
+        tracks = frame['MMCTrackList']
+        Tree_parent = Tree.parent
+
         primaries = Tree.primaries
-        primaries = [p for p in primaries if p.type in self.nu_set]
-        if(len(primaries)>1):
-            #Not the correct way to handle this. Don't expect this to happen so should throw an error
-            print("More than one neutrino primary! Using max energy neutrino for frame.")
-            primaries = [max(primaries, key=self.get_energy)]
-        daughters = [Tree.get_daughters(p.id) for p in primaries] #Get the daughters of the primaries
-        muons = [[d for d in p if(d.type == d.MuPlus or d.type == d.MuMinus)] for p in daughters] #Select only daughters that are muons
-        highEMuons = [max(p, key=self.get_energy) if len(p) > 0 else None for p in muons] #Choose only the highest energy muon
-        highEMuons = [(m if abs(m.pos) < 500 else None) if m != None else None for m in highEMuons] #Choose only muons that start within 500m of the detector center
-        highEMuons = [(m if m.energy >= 5000 else None) if m != None else None for m in highEMuons] #Choose only muons that are at least 5TeV
-        losses = [[d for d in Tree.get_daughters(m) if d.type in self.loss_set] if m != None else None for m in highEMuons] #Get the muon daughters
 
-        if(len(muons) > 1):
-            raise ValueError('There is more than one muon in the frame.')
+        tracks = [track for track in tracks if track.particle.type in mu_set] # Only tracks that are muons
+        tracks = [track for track in tracks if Tree.has(track.particle)] # Only tracks in MC tree
+        tracks = [track for track in tracks if Tree_parent(track.particle).type in nu_set] # Only tracks that have nu parent
+        tracks = [track for track in tracks if Tree_parent(track.particle) in primaries] # Only tracks that have primary parent
+        
+        tracks = [track for track in tracks if track.particle.energy > 5000] # Only muons that are at least 5TeV at creation
+        tracks = [track for track in tracks if abs(track.particle.pos) < 500] # Only muons that are created within 500m of the detector
 
+        nu_primaries_of_tracks = unique_list([Tree_parent(track.particle) for track in tracks])
+        tracks_by_primary = [[track for track in tracks if Tree_parent(track.particle) == p] for p in nu_primaries_of_tracks]
+        max_E_muons_by_primary = [max(tracks_for_primary, key=self.get_energy) for tracks_for_primary in tracks_by_primary]
+
+        n_muons = len(max_E_muons_by_primary)
+        
+        if(n_muons > 1):
+            raise ValueError('There is more than one muon in the frame')
+        
         HighEMuonLosses = dataclasses.I3VectorI3Particle()
-        if highEMuons[0] != None:
-            HighEMuonLosses.append(primaries[0])
-            HighEMuonLosses.append(highEMuons[0])
-            for loss in losses[0]:
+        HighEMuonTracks = simclasses.I3MMCTrackList()
+        if(n_muons > 0):
+            muon_track = max_E_muons_by_primary[0]
+            losses = [d for d in Tree.get_daughters(muon_track.particle) if d.type in loss_set] #Get the muon daughters
+            for loss in losses:
                 HighEMuonLosses.append(loss)
-        else:
-            del frame['I3MCWeightDict']
-        frame['HighEMuonLosses'] = HighEMuonLosses
-
-        self.PushFrame(frame)
+            frame['Primary'] = Tree_parent(muon_track.particle)
+            frame['Track'] = HighEMuonTracks
+            frame['Losses'] = HighEMuonLosses
+            self.PushFrame(frame)
 
 # Only look at files not already handled
-processed_files = get_processed_files(outdir)
-infiles = [file for indir in indirs for file in glob.glob('%s*.i3.bz2' % indir) if ntpath.basename(file) not in processed_files]
-
-infiles.sort()
+infiles = get_infiles(indirs, outdir)
 
 while(len(infiles)):
     try:
         # Pick the first N files to process
         bundle = infiles[:bundle_size]
         name_split = ntpath.basename(bundle[0]).split('.')
-        name_split[3]  = '%06.0f-%06.0f' % (lambda x: (min(x), max(x)))([int(ntpath.basename(infile).split('.')[3]) for infile in bundle])
-        outfile = outdir + '.'.join(name_split[:-2]) + '.h5'
+        name_split[3]  = '%06.0f-%06.0f' % (lambda x: (min(x), max(x)))([int(ntpath.basename(infile).split('.')[3]) for infile in bundle]) # File number after third period
+        outfile = outdir + '.'.join(name_split[:-2]) + '.h5' # Remove the two extensions and replace with .h5
 
         tray = I3Tray()
         hdftable = hdfwriter.I3HDFTableService(outfile)
         tray.Add("I3Reader", "my_reader", FilenameList=bundle)
         tray.Add(lambda frame: frame.Has('I3MCTree'))
+        tray.Add(lambda frame: frame.Has('I3MCWeightDict'))
+        tray.Add(lambda frame: frame.Has('MMCTrackList'))
         tray.Add(MyModule)
         
         tray.Add(tableio.I3TableWriter,'hdf1',
                  tableservice = hdftable,
                  SubEventStreams = ['InIce', 'InIceSplit'],
-                 keys = ['HighEMuonLosses', 'I3MCWeightDict']
+                 keys = ['Primary', 'Track', 'Losses', 'I3MCWeightDict']
                 )
         
         tray.Execute()
         tray.Finish()
+
     # Skip I3 files that have issues
     except RuntimeError as e:
         pass
     
-    processed_files = get_processed_files(outdir)
-    infiles = [file for indir in indirs for file in glob.glob('%s*.i3.bz2' % indir) if ntpath.basename(file) not in processed_files]
-    infiles.sort()
-    #bundle_start += bundle_size
+    infiles = get_infiles(indirs, outdir)
+
