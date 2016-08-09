8430ffd3c968ffd3389e6ded9d99de20d131493d
Austin Schneider <hogenshpogen@gmail.com>
2016-08-09 12:02:33 -0500

    nd_histogram. Track integration. Histogram aggregation.

--- mc-proc/muon_loss_common.py
+++ mc-proc/muon_loss_common.py
@@ -29,6 +29,7 @@ def memodict(f):
     return memodict().__getitem__
 
 def get_particle_number(arg):
+    """ Get the internal icecube particle number by its string name """
     if hasattr(arg, '__iter__'):
         s_list = arg
     elif type(arg) == str:
@@ -66,20 +67,45 @@ def get_is_in_sim_vol(sim_vol_length=1600, sim_vol_radius=800):
     is_in_sim_vol = lambda m,top=sim_vol_top,bot=sim_vol_bottom,r=sim_vol_radius,x=muon_p_pos_x,y=muon_p_pos_y,z=muon_p_pos_z: (m[z] < top and m[z] > bot and (m[x]**2 + m[y]**2)**(0.5) < r)
     return is_in_sim_vol
 
-class histogram:
+class histogram_nd(object):
     """ Histogram class for creating weighted average plot """
-    def __init__(self, bins):
+    def __init__(self, n, bins, store_data=False):
+        self.n = n
         self.bins = np.array(bins)
         self.size = len(bins) - 1
+        self.store_data = store_data
         size = self.size
         self.weights = np.zeros(size)
         self.weights2 = np.zeros(size)
         self.x_weighted = np.zeros(size)
-        self.y_weighted = np.zeros(size)
+        self.y_weighted = np.zeros((n, size))
         self.x2_weighted = np.zeros(size)
-        self.y2_weighted = np.zeros(size)
+        self.y2_weighted = np.zeros((n, size))
+        
+        if self.store_data:
+            self.x_data = []
+            self.y_data = np.zeros((n, 0)).tolist()
+            self.bin_data = []
+            self.weights_data = []
+        else:
+            self.x_data = None
+            self.y_data = None
+            self.bin_data = None
+            self.weights_data = None
     
     def add(self, x, y, weights, bin=None):
+        if self.store_data:
+            if len(y) != len(self.y_data):
+                raise ValueError("y length must match y_data!")
+            self.x_data += list(x)
+            for i in  xrange(len(y)):
+                self.y_data[i] += list(y[i])
+            self.weights_data += list(weights)
+            if bin:
+                self.bin_data += bin
+            else:
+                self.bin_data += ([None]*len(y))
+        
         x = np.array(x)
         y = np.array(y)
         weights = np.array(weights)
@@ -89,16 +115,22 @@ class histogram:
             weights_by_bin = np.bincount(bin, minlength=self.size, weights=weights)
             weights2_by_bin = np.bincount(bin, minlength=self.size, weights=weights**2)
             x_weighted_by_bin = np.bincount(bin, minlength=self.size, weights=x * weights)
-            y_weighted_by_bin = np.bincount(bin, minlength=self.size, weights=y * weights)
             x2_weighted_by_bin = np.bincount(bin, minlength=self.size, weights=x**2 * weights)
-            y2_weighted_by_bin = np.bincount(bin, minlength=self.size, weights=y**2 * weights)
+            y_weighted_by_bin = np.zeros((self.n, self.size))
+            y2_weighted_by_bin = np.zeros((self.n, self.size))
+            for i in xrange(len(y)):
+                y_weighted_by_bin[i] = np.bincount(bin, minlength=self.size, weights=y[i] * weights)
+                y2_weighted_by_bin[i] = np.bincount(bin, minlength=self.size, weights=y[i]**2 * weights)
         else:
             weights_by_bin, edges = np.histogram(x, bins=self.bins, weights=weights)
             weights2_by_bin, edges = np.histogram(x, bins=self.bins, weights=weights**2)
             x_weighted_by_bin, edges = np.histogram(x, bins=self.bins, weights=x * weights)
-            y_weighted_by_bin, edges = np.histogram(x, bins=self.bins, weights=y * weights)
             x2_weighted_by_bin, edges = np.histogram(x, bins=self.bins, weights=x**2 * weights)
-            y2_weighted_by_bin, edges = np.histogram(x, bins=self.bins, weights=y**2 * weights)
+            y_weighted_by_bin = np.zeros((self.n, self.size))
+            y2_weighted_by_bin = np.zeros((self.n, self.size))
+            for i in xrange(len(y)):
+                y_weighted_by_bin[i], edges = np.histogram(x, bins=self.bins, weights=y[i] * weights)
+                y2_weighted_by_bin[i], edges = np.histogram(x, bins=self.bins, weights=y[i]**2 * weights)
 
         self.weights += weights_by_bin
         self.weights2 += weights2_by_bin
@@ -106,7 +138,31 @@ class histogram:
         self.y_weighted += y_weighted_by_bin
         self.x2_weighted += x2_weighted_by_bin
         self.y2_weighted += y2_weighted_by_bin
+
+        return {'w': weights_by_bin, 'w2': weights2_by_bin, 'x': x_weighted_by_bin, 'y': y_weighted_by_bin, 'x2': x2_weighted_by_bin, 'y2': y2_weighted_by_bin}
+
+    def accumulate(self, hist):
+        if not isinstance(hist, histogram_nd):
+            raise ValueError("Trying to accumulate non histogram object")
+        if not hist.n == self.n:
+            raise ValueError("Histogram must have same dimension")
+        same_shape = (self.weights.shape == hist.weights.shape and
+            self.weights2.shape == hist.weights2.shape and
+            self.x_weighted.shape == hist.x_weighted.shape and
+            self.y_weighted.shape == hist.y_weighted.shape and
+            self.x2_weighted.shape == hist.x2_weighted.shape and
+            self.y2_weighted.shape == hist.y2_weighted.shape)
+        if not same_shape:
+            raise ValueError("Histogram shapes are not the same")
         
+        self.weights += hist.weights
+        self.weights2 += hist.weights2
+        self.x_weighted += hist.x_weighted
+        self.y_weighted += hist.y_weighted
+        self.x2_weighted += hist.x2_weighted
+        self.y2_weighted += hist.y2_weighted
+
+
     def get_w(self):
         w = self.weights[:]
         w[w == 0] = 1
@@ -118,13 +174,17 @@ class histogram:
     def get_x(self):
         return self.x_weighted / self.get_w()
     def get_y(self):
-        return self.y_weighted / self.get_w()
+        w = self.get_w()
+        y = self.y_weighted / ([w]*self.n)
+        return y
     def get_x2(self):
         return self.x2_weighted / self.get_w()
     def get_y2(self):
-        return self.y2_weighted / self.get_w()
+        w = self.get_w()
+        y2 = self.y2_weighted / ([w]*self.n)
+        return y2
     def stddev(self, x, x2):
-        variance = np.zeros(self.size)
+        variance = np.zeros(x.shape)
         good_var = np.logical_not(np.logical_or(np.isclose(x2, x**2, rtol=1e-05, atol=1e-09), x2 < x**2))
         variance[good_var] = x2[good_var] - x[good_var]**2
         return np.sqrt(variance)
@@ -133,7 +193,23 @@ class histogram:
     def get_y_stddev(self):
         return self.stddev(self.get_y(), self.get_y2())
     def get_x_stddev_of_mean(self):
-        return self.get_x_stddev() * np.sqrt(self.get_w2()) / self.get_w()
+        return self.get_x_stddev() * np.sqrt(self.get_w2()) / self.get_w() 
+    def get_y_stddev_of_mean(self):
+        return self.get_y_stddev() * ([np.sqrt(self.get_w2()) / self.get_w()]*self.n)
+
+class histogram(histogram_nd):
+    """ Histogram class for creating weighted average plot """
+    def __init__(self, bins, store_data=False):
+        super(histogram, self).__init__(1, bins, store_data)
+
+    def add(self, x, y, weights, bin=None):
+        return super(histogram, self).add(x, [y], weights, bin)
+    def get_y(self):
+        return super(histogram, self).get_y()[0]
+    def get_y2(self):
+        return super(histogram, self).get_y2()[0]
+    def get_y_stddev(self):
+        return super(histogram, self).get_y_stddev()[0]
     def get_y_stddev_of_mean(self):
         return self.get_y_stddev() * np.sqrt(self.get_w2()) / self.get_w()
 
@@ -205,6 +281,9 @@ def get_loss_info(stuff):
     return (loss_rate, losses)
 
 def get_bounding_elements(x, l, key=lambda elem: elem, sort=False):
+    """
+    Get the two elements of a list that bound a value
+    """
     if sort:
         l = sorted(l, key=key)
     return (next(itertools.dropwhile(lambda elem: key(elem) > x, reversed(l)), None),
@@ -256,6 +335,24 @@ def get_energy(x, checkpoints, loss_tuples, inclusive=True, has_sum=True):
 
     return energy
 
+def integrate_track_energy(x1, x2, checkpoints, loss_tuples, has_sum=True):
+    """
+    Integrate the track energy between two points
+    """
+    keep_point = lambda p: (p[1] > x1) and (p[1] < x2)
+    points = sorted([cp[1] for cp in checkpoints if keep_point(cp)] + [loss[1] for loss in loss_tuples if keep_point(loss)] + [x2])
+
+    area = 0
+
+    E1 = get_energy(x1, checkpoints, loss_tuples, inclusive=True, has_sum=has_sum)
+    for x2 in points:
+        E2 = get_energy(x2, checkpoints, loss_tuples, inclusive=False, has_sum=has_sum)
+        area += (x2 - x1)*(E1 + E2)/2.0
+        x1 = x2
+        E1 = get_energy(x1, checkpoints, loss_tuples, inclusive=True, has_sum=has_sum)
+
+    return area
+
 def get_info_from_file(infile):
     inpickle = open(infile, 'rb')
     hists = pickle.load(inpickle)
@@ -264,6 +361,24 @@ def get_info_from_file(infile):
     inpickle.close()
 
     return (hists, bins)
+
+def aggregate_info_from_dirs(dirs):
+    aggregated_hists = None
+    aggregated_bins = None
+    for dir in dirs:
+        files = glob.glob(dir + "*.pkl")
+        for file in files:
+            hists, bins = get_info_from_file(file)
+            if aggregated_hists is None:
+                aggregated_hists = hists
+                aggregated_bins = bins
+            else:
+                if (aggregated_bins == bins).all() and len(hists) == len(aggregated_hists):
+                    for i in xrange(len(hists)):
+                        aggregated_hists[i].accumulate(hists[i])
+                else:
+                    raise ValueError("Histograms must match!")
+    return aggregated_hists, aggregated_bins
 
 def save_info_to_file(outfile, hists, bins):
     outpickle = open(outfile, 'wb')
